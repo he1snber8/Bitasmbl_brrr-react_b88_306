a37f9df94846e9dd663c1e05993401d31f347b37
lukakhaja <lukakhaja@yahoo.com>
2026-05-01 21:58:40 +0400

    bitasmbl add: Define portfolio layout

--- frontend/src/pages/pagehah.tsx
+++ frontend/src/pages/pagehah.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function pagehah() {
-  return <div>menacing lolita, shes a universe,jigga, bruh</div>;
+  return <div>menacing lolita, shes a universe,jigga, bruh,haha</div>;
 }
